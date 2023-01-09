a28ac0b96552099915687acba0ec1a6413d2e2d8
mw-surplex <martin.wiesner@surplex.com>
2023-01-09 16:48:17 +0000

    Fix QferrerMandrillBundle

--- src/QferrerMandrillBundle.php
+++ src/QferrerMandrillBundle.php
@@ -4,6 +4,7 @@ namespace Qferrer\Symfony\MandrillBundle;
 
 use Qferrer\Symfony\MandrillBundle\DependencyInjection\MandrillExtension;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 
 class QferrerMandrillBundle extends Bundle
 {
